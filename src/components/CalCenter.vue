2c067a31368c32573676463f26b1c07defb8b84e
武为江 <by_wwj@163.com>
2018-05-10 19:41:41 +0800

    第一步。

--- src/components/CalCenter.vue
+++ src/components/CalCenter.vue
@@ -4,12 +4,26 @@
           <tab-item selected @on-item-click="onItemClick">第一步</tab-item>
           <tab-item @on-item-click="onItemClick">第二步</tab-item>
         </tab>
+      <div id="firstStep">
+        <group>
+          <calendar @on-change="onChange" v-model="demo3" title="开标时间：" disable-future></calendar>
+        </group>
+
+        <group>
+        <picker title="项目编号：" :data='years001' v-model='year001' @on-change='change'></picker>
+        </group>
+        <br>
+
+      </div>
+      <div id="secondStep">
+
+      </div>
     </div>
+
 </template>
 
-
 <script>
-  import { Tab, TabItem, Sticky, Divider, XButton, Swiper, SwiperItem } from 'vux'
+  import { Tab, TabItem, Sticky, Divider, XButton, Swiper, SwiperItem,Calendar, Badge,Group, Picker} from 'vux'
     export default {
       components: {
         Tab,
@@ -18,10 +32,18 @@
         Divider,
         XButton,
         Swiper,
-        SwiperItem
+        SwiperItem,
+        Calendar,
+        Badge,
+        Group,
+        Picker
       },
       data() {
-
+        return {
+          demo3: 'TODAY',
+          years001: ['a','b','c','d'],
+          year001: ['a'],
+        }
       },
       methods: {
         onItemClick (index) {
@@ -34,23 +56,8 @@
 <style lang="less" scoped>
   @import '~vux/src/styles/1px.less';
   @import '~vux/src/styles/center.less';
-  .box {
-    padding: 15px;
-  }
-  .active-6-1 {
-    color: rgb(252, 55, 140) !important;
-    border-color: rgb(252, 55, 140) !important;
-  }
-  .active-6-2 {
-    color: #04be02 !important;
-    border-color: #04be02 !important;
-  }
-  .active-6-3 {
-    color: rgb(55, 174, 252) !important;
-    border-color: rgb(55, 174, 252) !important;
-  }
-  .tab-swiper {
-    background-color: #fff;
-    height: 100px;
+  .selected-days {
+    color: #999;
+    width: 90px;
   }
 </style>
--- src/components/HelloFromVux.vue
+++ src/components/HelloFromVux.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
+    <x-header left-options.showBack>This is the page title.</x-header>
+    <br>
+    <div>
     <router-view></router-view>
+    </div>
     <tabbar>
-      <tabbar-item  link="/userCenter">
+      <tabbar-item  @on-item-click="toUserCenter">
         <span slot="label" >用户中心</span>
       </tabbar-item>
-      <tabbar-item  link="/calCenter">
+      <tabbar-item   @on-item-click="toCalCenter">
         <span slot="label">计算中心</span>
       </tabbar-item>
     </tabbar>
@@ -13,11 +17,31 @@
 </template>
 
 <script>
+  import { XHeader, Actionsheet, TransferDom, ButtonTab, ButtonTabItem } from 'vux'
 export default {
+  directives: {
+    TransferDom
+  },
+  components: {
+    XHeader,
+    Actionsheet,
+    ButtonTab,
+    ButtonTabItem
+  },
   methods: {
-
+    toUserCenter()  {
+      this.$router.push("/userCenter")
+    },
+    toCalCenter()  {
+      this.$router.push("/calCenter")
+    }
   },//end methods
   data () {
+  },
+  watch: {
+    '$route': function (newV) {
+      this.actived = this.tabList.findIndex(i => i.path === newV.path)
+    }
   }
 }
 </script>
